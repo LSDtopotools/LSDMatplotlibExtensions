284ddf186561fb561ddb54bdea467123e77a4a53
decvalts <declan.valters@gmail.com>
2017-01-14 11:41:48 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='lsdmatplotlibextensions',
+      version='0.1',
+      description='Extensions to matplotlib for the LSDPlotting and LSDMappingTools scripts',
+      url='http://github.com/LSDTopoTools/LSDMatplotlibExtensions',
+      author='LSDTopoTools',
+      author_email='dvalts@gmail.com',
+      license='GNU GPL v3',
+      packages=['lsdmatplotlibextensions'],
+      zip_safe=False)
+
